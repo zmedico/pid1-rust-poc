d69975aea753f8f102dfafd21706bf21024fbc97
Zac Medico <zmedico@gmail.com>
2020-01-01 16:23:55 -0800

    Handle SIGCHLD with tokio::signal::unix::signal

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,19 @@
-use async_std::prelude::*;
-use async_std::sync::{channel, Receiver};
 
-struct Zombies {
-    receiver: Receiver<()>,
-    sigid: signal_hook::SigId,
-}
+async fn reap_till(till: i32) -> Result<(), Pid1Error> {
+    let mut status = 0;
+    let mut stream = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::child())?;
 
-impl Drop for Zombies {
-    fn drop(&mut self) {
-        signal_hook::unregister(self.sigid);
-    }
-}
-
-impl Zombies {
-    fn new() -> Result<Self, std::io::Error> {
-        let (sender, receiver) = channel(1);
-
-        let handler = move || {
-            let sender_clone = sender.clone();
-
-            async_std::task::spawn(async move {
-                sender_clone.send(()).await;
-            });
-            ()
-        };
-        let sigid = unsafe { signal_hook::register(signal_hook::SIGCHLD, handler)? };
-        Ok(Zombies { receiver, sigid })
-    }
-
-    async fn reap_till(self, till: i32) -> Result<(), Pid1Error> {
-        while let Some(()) = self.receiver.recv().await {
-            let mut status = 0;
-            loop {
-                let pid = unsafe { libc::waitpid(-1, &mut status, libc::WNOHANG) };
-                if pid == till {
-                    return Ok(());
-                } else if pid <= 0 {
-                    break;
-                }
+    while stream.recv().await.is_some() {
+        loop {
+            let pid = unsafe { libc::waitpid(-1, &mut status, libc::WNOHANG) };
+            if pid == till {
+                return Ok(());
+            } else if pid <= 0 {
+                break;
             }
         }
-        Ok(())
     }
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -57,7 +29,7 @@ impl std::convert::From<std::io::Error> for Pid1Error {
     }
 }
 
-#[async_attributes::main]
+#[tokio::main]
 async fn main() -> Result<(), Pid1Error> {
     let (cmd, args) = get_command()?;
     let child = std::process::Command::new(cmd).args(args).spawn()?.id();
@@ -76,7 +48,7 @@ async fn main() -> Result<(), Pid1Error> {
     let sigid: signal_hook::SigId =
         unsafe { signal_hook::register(signal_hook::SIGINT, interrupt_child)? };
 
-    Zombies::new()?.reap_till(child).await?;
+    reap_till(child).await?;
 
     signal_hook::unregister(sigid);
     Ok(())
